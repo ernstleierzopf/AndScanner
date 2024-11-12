3634b8a398964fb62666412f88b393a3318b005a
Ernst Leierzopf <ernst.leierzopf@ins.jku.at>
2024-11-12 10:29:20 +0100

    fix metadata output.

--- romanalyzer_extractor/extractor/metadata.py
+++ romanalyzer_extractor/extractor/metadata.py
@@ -30,7 +30,7 @@ class MetadataExtractor(Extractor):
                 for line in output_lines:
                     line = line.strip()
                     if line.startswith("Build ID: "):
-                        metadata += line.replace("Build ID: ", "")
+                        metadata += line.replace("Build ID: ", "") + ";"
                     if "Shared library: [" in line:
                         libraries.append(line.split("Shared library: [")[1][:-1])
                 metadata += ", ".join(libraries)
